5fec060be41f824a932847bffef2fe0cca5970e7
unknown <shukhratgadoev@gmail.com>
2020-08-27 04:09:47 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Server } from "miragejs";
+import { Provider as MobXProvider } from 'mobx-react';
+
+import booksStore from '../stores/BooksStore';
+
+
+const stores = {
+  booksStore,
+};
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MobXProvider {...stores}>
+      <App />
+  </MobXProvider>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
